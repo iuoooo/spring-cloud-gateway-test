dce7bf4d00efb538c12fb63230d9363de6b28b8a
iuoooo <javalijingkui@163.com>
2020-01-17 10:57:27 +0800

    初始化gateway项目，完成 http:localhost://8080/baidu--->https://www.baidu.com

---
+++ gateway-test/src/main/java/com/iuoooo/GatewayTestApp.java
@@ -0,0 +1,13 @@
+package com.iuoooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GatewayTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayTestApp.class, args);
+    }
+
+}
